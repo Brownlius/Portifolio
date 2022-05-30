a8d1637e1f7cc38e87dd0f053dc6823ff207d8d6
Pedro Almeida <p.almeida@live.com>
2022-05-30 08:44:42 -0400

    Apagado pasta não utilizada

--- Site-inicial/Projetos/Freeway/Script/index.js
+++ Site-inicial/Projetos/Freeway/Script/index.js
@@ -1,32 +1,25 @@
 let tela;
 let ctx;
-let fundoimg = new Image();
-
 window.onload = function (){
     geraCanvas();
     update();
-    fundo();
+    
 }
 function fundo(){
-    fundoimg.src = "Site-inicial/Projetos/FreeWay 2.0/Imagens/estrada.png";
-    ctx.drawImage(fundoimg, 0, 0);  
+    
+    let fundoimg = new Image();
+    fundoimg.src = "Imagens/estrada.png";
+    ctx.drawImage(fundoimg, 0, 0, tela.width, tela.height);  
 }
 
 function geraCanvas(){
     tela = document.getElementById("canvas");
+    ctx = tela.getContext("2d");
     tela.width = 1920/1.5;
     tela.height = 1080/1.5;
-    ctx = tela.getContext("2d");
 }
 
 function update(){
-    // desenhaCanvas();
-    
+    fundo();
 }
-
-// function desenhaCanvas(){
-//     ctx.fillStyle = "black";
-//     ctx.fillRect(0,0, tela.width, tela.height);
-
-// }
 
